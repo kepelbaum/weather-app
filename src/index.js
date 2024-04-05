0c312f0a66e8a3804c980c449b0c5e5d7ee78129
Kirill Epelbaum <150293425+kepelbaum@users.noreply.github.com>
2024-04-04 23:02:44 -0400

    WIP

--- src/index.js
+++ src/index.js
@@ -7,15 +7,14 @@ let img = document.querySelector('img');
 
 async function request(city) {
     try {
-    let link = 'https://api.weatherapi.com/v1/current.json?key=46523e6a6f0346c2bd412410240504&q=' + city;
+    let link = 'http://api.weatherapi.com/v1/current.json?key=46523e6a6f0346c2bd412410240504&q=' + city;
     const response = await fetch(link, {mode: 'cors'});
     const result = await response.json();
     temp.textContent = city + ': ' + result.current.temp_c + 'C';
     hum.textContent = 'Humidity: ' + result.current.humidity + '%';
     wind.textContent = 'Wind: ' + result.current.wind_kph + 'kph';
     cond.textContent = result.current.condition.text;
-    img.src = 'https:' + result.current.condition.icon;
-    console.log(result);
+    img.src = 'http:' + result.current.condition.icon;
     } catch (error) {
         console.log(error);
     }
